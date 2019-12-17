f24de0c5a045a49d1c4056d9eba35baef70a844e
Andrew Le <andrewandyle@gmail.com>
2019-12-17 16:03:27 -0600

    Updated top slideshow to use JavaScript

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ var scroll = new SmoothScroll('a[href*="#"]', {
   offset: 75,
   speed: 500
 });
+
+// slideshow for showcase
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("showcase-img");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 8000); // Change image every 8 seconds
+}
