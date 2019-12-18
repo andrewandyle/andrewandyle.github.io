ca10890874e6fcacc2566d031957ad5effb1cf9d
Andrew Le <andrewandyle@gmail.com>
2019-12-18 17:00:31 -0600

    Changed slideshow to swipe transition

--- js/script.js
+++ js/script.js
@@ -21,31 +21,69 @@ function showcaseSlides() {
   setTimeout(showcaseSlides, 8000); // Change image every 8 seconds
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// slideshow for personal life
+const _C = document.querySelector('.personal-slideshow'),
+      N = _C.children.length;
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+_C.style.setProperty('--n', N)
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("life-slide");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+let x0 = null;
+
+let locked = false;
+
+function lock(e) {
+  x0 = unify(e).clientX;
+  _C.classList.toggle('smooth', !(locked = true))
+};
+
+let i = 0;
+
+let w;
+
+function size() { w = window.innerWidth };
+
+function move(e) {
+  if(locked) {
+    let dx = unify(e).clientX - x0, s = Math.sign(dx),
+        f = +(s*dx/w).toFixed(2);
+
+    if((i > 0 || s < 0) && (i < N - 1 || s > 0) && f > .2) {
+      _C.style.setProperty('--i', i -= s);
+      f = 1 - f
+    }
+
+    _C.style.setProperty('--tx', '0px');
+    _C.style.setProperty('--f', f);
+    _C.classList.toggle('smooth', !(locked = false));
+    x0 = null
+
+    var dots = document.getElementsByClassName("dot");
+    for (var j = 0; j < dots.length; j++) {
+        dots[j].className = dots[j].className.replace(" active", "");
+    }
+    dots[i].className += " active";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+};
+
+size();
+
+addEventListener('resize', size, false);
+
+_C.addEventListener('mousedown', lock, false);
+_C.addEventListener('touchstart', lock, false);
+
+_C.addEventListener('mouseup', move, false);
+_C.addEventListener('touchend', move, false);
+
+function drag(e) {
+  e.preventDefault();
+
+  if(locked)
+    _C.style.setProperty('--tx', `${Math.round(unify(e).clientX - x0)}px`)
+};
+
+_C.addEventListener('mousemove', drag, false);
+_C.addEventListener('touchmove', drag, false);
+
+function unify(e) { return e.changedTouches ? e.changedTouches[0] : e };
