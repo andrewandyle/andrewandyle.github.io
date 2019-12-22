667a2c4cf9d83465f95cb7c3cd54342e2c16975f
Andrew Le <andrewandyle@gmail.com>
2019-12-21 20:30:23 -0600

    Made sidebar simply go over page

--- js/project-page.js
+++ js/project-page.js
@@ -5,14 +5,12 @@ var scroll = new SmoothScroll('a[href*="#"]', {
   speed: 100
 });
 
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+/* Set the width of the sidebar to 420px */
 function openNav() {
   document.getElementById("sidebar").style.width = "420px";
-  document.getElementById("main").style.marginLeft = "420px";
 }
 
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+/* Set the width of the sidebar to 0 */
 function closeNav() {
   document.getElementById("sidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
 }
